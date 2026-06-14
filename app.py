ca359990645fabc0b2ef854e41675d08b004dfaf
Karan Kokane <karankumarkokane@gmail.com>
2026-06-14 23:00:13 +0530

    Initial attendance system

---
+++ app.py
@@ -0,0 +1,195 @@
+
+
+
+from flask import (
+    Flask,
+    render_template,
+    request
+)
+
+from database import (
+    login_employee,
+    mark_punch_in,
+    mark_punch_out,
+    check_location,
+    already_punched_today,
+    save_photo_url,
+    save_photo_out_url,
+    upload_photo,
+    get_today_attendance
+)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+
+    return render_template(
+        "login.html"
+    )
+
+
+@app.route("/login", methods=["POST"])
+def login():
+
+    username = request.form["username"]
+    password = request.form["password"]
+
+    employee = login_employee(
+        username,
+        password
+    )
+
+    if employee:
+    
+        attendance = get_today_attendance(
+                employee["id"]
+        )
+    
+        return render_template(
+            "dashboard.html",
+            employee=employee,
+            attendance=attendance
+        )
+    
+    return "Invalid username or password"
+
+
+@app.route("/punch_in", methods=["POST"])
+def punch_in():
+
+    data = request.json
+
+    employee_id = int(
+        data["employee_id"]
+    )
+
+    latitude = data["latitude"]
+    longitude = data["longitude"]
+    photo = data["photo"]
+    
+    employee = (
+        login_employee(
+            "karan",
+            "1234"
+        )
+    )
+
+    allowed = check_location(
+        employee,
+        latitude,
+        longitude
+    )
+
+    if not allowed:
+
+        return (
+            "❌ Outside office location"
+        )
+
+    if already_punched_today(
+            employee_id):
+    
+        return (
+            "⚠️ Already punched in today"
+        )
+    
+    mark_punch_in(
+        employee_id
+    )
+    
+    photo_url = upload_photo(
+        employee_id,
+        photo
+    )
+    
+    save_photo_url(
+        employee_id,
+        photo_url
+    )
+    
+    attendance = (
+        get_today_attendance(
+            employee_id
+        )
+    )
+    
+    return render_template(
+        "dashboard.html",
+        employee=employee,
+        attendance=attendance
+    )
+
+@app.route(
+    "/punch_out",
+    methods=["POST"]
+)
+def punch_out():
+
+    data = request.json
+
+    employee_id = int(
+        data["employee_id"]
+    )
+
+    latitude = data["latitude"]
+    longitude = data["longitude"]
+
+    photo = data["photo"]
+
+    employee = (
+        login_employee(
+            "karan",
+            "1234"
+        )
+    )
+
+    allowed = check_location(
+        employee,
+        latitude,
+        longitude
+    )
+
+    if not allowed:
+
+        return (
+            "❌ Outside office location"
+        )
+
+    result = mark_punch_out(
+        employee_id
+    )
+
+    if result != (
+        "Punch out successful"
+    ):
+        return result
+
+    photo_url = upload_photo(
+        employee_id,
+        photo
+    )
+
+    save_photo_out_url(
+        employee_id,
+        photo_url
+    )
+
+    attendance = (
+        get_today_attendance(
+            employee_id
+        )
+    )
+    
+    return render_template(
+        "dashboard.html",
+        employee=employee,
+        attendance=attendance
+    )
+
+if __name__ == "__main__":
+    app.run(
+        debug=True,
+        use_reloader=False
+    )
---
+++ database.py
@@ -0,0 +1,323 @@
+
+from geopy.distance import geodesic
+import base64
+from datetime import datetime
+from supabase import create_client
+from dotenv import load_dotenv
+import os
+from datetime import date
+
+# Load environment variables
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+supabase = create_client(
+    SUPABASE_URL,
+    SUPABASE_KEY
+)
+
+
+def login_employee(username, password):
+
+    response = supabase.table(
+        "employees"
+    ).select(
+        "*"
+    ).eq(
+        "username", username
+    ).eq(
+        "password", password
+    ).eq(
+        "is_active", True
+    ).execute()
+
+    if len(response.data) > 0:
+        return response.data[0]
+
+    return None
+
+
+def mark_punch_in(employee_id):
+
+    today = str(
+        date.today()
+    )
+
+    current_time = str(
+        datetime.now()
+    )
+
+    response = supabase.table(
+        "attendance"
+    ).insert(
+        {
+            "employee_id":
+                employee_id,
+
+            "attendance_date":
+                today,
+
+            "punch_in":
+                current_time,
+
+            "status":
+                "Present"
+        }
+    ).execute()
+
+    return response
+
+def check_location(employee, user_lat, user_long):
+
+    office_location = (
+        employee["latitude"],
+        employee["longitude"]
+    )
+
+    employee_location = (
+        user_lat,
+        user_long
+    )
+
+    distance = geodesic(
+        office_location,
+        employee_location
+    ).meters
+
+    print(
+        "Distance:",
+        distance
+    )
+
+    if distance <= employee["allowed_radius"]:
+        return True
+
+    return False
+
+def already_punched_today(
+        employee_id):
+
+    today = str(
+        date.today()
+    )
+
+    response = supabase.table(
+        "attendance"
+    ).select(
+        "*"
+    ).eq(
+        "employee_id",
+        employee_id
+    ).eq(
+        "attendance_date",
+        today
+    ).execute()
+
+    if len(
+        response.data
+    ) > 0:
+
+        return True
+
+    return False
+
+def mark_punch_out(
+        employee_id):
+
+    today = str(
+        date.today()
+    )
+
+    response = supabase.table(
+        "attendance"
+    ).select(
+        "*"
+    ).eq(
+        "employee_id",
+        employee_id
+    ).eq(
+        "attendance_date",
+        today
+    ).execute()
+
+    if len(
+        response.data
+    ) == 0:
+
+        return (
+            "No punch in found"
+        )
+
+    attendance = (
+        response.data[0]
+    )
+
+    if attendance[
+            "punch_out"]:
+
+        return (
+            "Already punched out"
+        )
+
+    punch_in_time = (
+        datetime.fromisoformat(
+            attendance[
+                "punch_in"
+            ]
+        )
+    )
+
+    punch_out_time = (
+        datetime.now()
+    )
+
+    total_hours = (
+        punch_out_time
+        -
+        punch_in_time
+    ).total_seconds() / 3600
+
+
+    supabase.table(
+        "attendance"
+    ).update(
+        {
+            "punch_out":
+                str(
+                    punch_out_time
+                ),
+
+            "total_hours":
+                round(
+                    total_hours,
+                    2
+                )
+        }
+    ).eq(
+        "id",
+        attendance["id"]
+    ).execute()
+
+    return (
+        "Punch out successful"
+    )
+
+
+def save_photo_url(
+        employee_id,
+        photo_url):
+
+    today = str(
+        date.today()
+    )
+
+    supabase.table(
+        "attendance"
+    ).update(
+        {
+            "photo_in":
+                photo_url
+        }
+    ).eq(
+        "employee_id",
+        employee_id
+    ).eq(
+        "attendance_date",
+        today
+    ).execute()
+        
+        
+def upload_photo(
+        employee_id,
+        image_data):
+
+    base64_data = (
+        image_data
+        .split(",")[1]
+    )
+
+    image_bytes = (
+        base64.b64decode(
+            base64_data
+        )
+    )
+
+    file_name = (
+        f"{employee_id}_"
+        f"{datetime.now().timestamp()}"
+        ".jpg"
+    )
+
+    supabase.storage.from_(
+        "attendance-photos"
+    ).upload(
+        file_name,
+        image_bytes
+    )
+
+    photo_url = (
+        supabase.storage
+        .from_(
+            "attendance-photos"
+        )
+        .get_public_url(
+            file_name
+        )
+    )
+
+    return photo_url
+
+def save_photo_out_url(
+        employee_id,
+        photo_url):
+
+    today = str(
+        date.today()
+    )
+
+    supabase.table(
+        "attendance"
+    ).update(
+        {
+            "photo_out":
+                photo_url
+        }
+    ).eq(
+        "employee_id",
+        employee_id
+    ).eq(
+        "attendance_date",
+        today
+    ).execute()
+        
+        
+def get_today_attendance(
+        employee_id):
+
+    today = str(
+        date.today()
+    )
+
+    response = supabase.table(
+        "attendance"
+    ).select(
+        "*"
+    ).eq(
+        "employee_id",
+        employee_id
+    ).eq(
+        "attendance_date",
+        today
+    ).execute()
+
+    if len(
+        response.data
+    ) > 0:
+
+        return (
+            response.data[0]
+        )
+
+    return None
